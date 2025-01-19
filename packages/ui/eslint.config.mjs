08068d952796837b6eae65c547c4cbe2130fe677
Drizzt <vbcc2001@gmail.com>
2025-01-19 21:19:45 +0800

    1

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@tranyu_nextjs_starter/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
