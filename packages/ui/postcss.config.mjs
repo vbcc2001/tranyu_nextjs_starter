e1bdbbd5c77092cf27951e8b46c73e43d113ab03
Drizzt <vbcc2001@gmail.com>
2025-01-26 22:22:11 +0800

    1

--- packages/ui/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  plugins: {
-    /** 用于处理 TailwindCSS 的核心功能，
-     * 在 v4 中，导入和供应商前缀现在会自动为你处理，
-     * 因此你可以删除 postcss-import 和 autoprefixer（如果它们在你的项目中）
-     */
-    "@tailwindcss/postcss": {}, 
-  },
-};
