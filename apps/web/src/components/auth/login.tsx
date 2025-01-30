9def21330255f993f233625b575aa4d510eae1f9
Drizzt <vbcc2001@gmail.com>
2025-01-30 11:37:18 +0800

    1

--- apps/web/src/components/auth/login.tsx
+++ apps/web/src/components/auth/login.tsx
@@ -17,9 +17,9 @@ export function LoginForm({
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back 欢迎回来</h1>
+                <h1 className="text-2xl font-bold">{AppConfig.title}</h1>
                 <p className="text-balance text-muted-foreground">
-                  登录你的{AppConfig.title}账户
+                  登录您的账户
                 </p>
               </div>
               <div className="grid gap-2">
@@ -27,7 +27,7 @@ export function LoginForm({
                 <Input
                   id="email"
                   type="email"
-                  placeholder="m@example.com"
+                  placeholder="name@tranyu.com"
                   required
                 />
               </div>
@@ -44,11 +44,11 @@ export function LoginForm({
                 <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                登录
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
-                  Or continue with
+                  使用其他账户
                 </span>
               </div>
               <div className="grid grid-cols-3 gap-4">
@@ -81,9 +81,9 @@ export function LoginForm({
                 </Button>
               </div>
               <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
+                还没有账户?{" "}
                 <a href="#" className="underline underline-offset-4">
-                  Sign up
+                  立即注册
                 </a>
               </div>
             </div>
@@ -91,8 +91,7 @@ export function LoginForm({
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        点击“继续”，即表示您同意我们的{" "}<a href="#">服务条款</a>{" "}和{" "}<a href="#">隐私政策</a>。
       </div>
     </div>
   )
