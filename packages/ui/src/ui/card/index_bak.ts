d66ae49baeabe7e423da8e6258a87de877d0e3da
Drizzt <vbcc2001@gmail.com>
2025-01-30 10:23:02 +0800

    1

--- packages/ui/src/ui/card/index_bak.ts
+++
@@ -1,9 +0,0 @@
-import Card from "./card"
-import Header from "./header"
-import Title from "./title"
-import Description from "./description"
-import Content from "./content"
-import Footer from "./footer"
-import Demo from "./demo"
-
-export { Card, Header, Title, Description, Content, Footer, Demo }
